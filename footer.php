df04f9cda948018103db9a497465cd0eef4cf6d5
LoeiFy <loeify@gmail.com>
2015-07-31 08:45:53 +0800

    readme

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer" class="pt20 rel w1 center">
-    <p class="w1 rel pt20 mt30 center">&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?> All Rights Reserved. Powered by WordPress · <a href="/wp-admin/">后台管理</a><span class="icon-resize-shrink" title="关闭页面" id="close"></span><span title="返回顶部" class="icon-arrow-up" id="top"></span></p>
+    <p class="w1 rel pt20 mt30 center">&copy; <?php echo date("Y"); ?> <?php bloginfo('name'); ?> All Rights Reserved. Powered by WordPress<span class="icon-resize-shrink" title="关闭页面" id="close"></span><span title="返回顶部" class="icon-arrow-up" id="top"></span></p>
 </div>
 
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
